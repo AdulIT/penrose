d2013b498d6e999bd32c6935e29ba1f913cc3a12
AdulIt <spilberg.max@bk.ru>
2022-12-09 11:40:25 +0600

    feat: slider, dropdown

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const dropDownBtn = document.querySelector('.menu__dropdown_btn');
 const dropDownList = document.querySelector('.menu__dropdown_list');
 const dropDownListItems = dropDownList.querySelectorAll('.menu__dropdown_list-item');
 
+
 dropDownBtn.addEventListener('click', function() {
     dropDownList.classList.toggle('menu__dropdown_list--visible');
 });
@@ -25,3 +26,26 @@ document.addEventListener('keydown', function (e) {
         dropDownList.classList.remove('menu__dropdown_list--visible');
     }
 });
+
+new Swiper('.main__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoHeight: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+});
+
+async function getResponse() {
+    let res = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
+    let data = await res.json();
+
+    console.log(data);
+}
+
+getResponse();
