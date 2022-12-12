cee6d161f6f8ecae9f694eb1d76e0a898490d9cf
AdulIt <spilberg.max@bk.ru>
2022-12-12 21:52:44 +0600

    feat: fetch from dummyjson

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const dropDownBtn = document.querySelector('.menu__dropdown_btn'),
       menu = document.querySelector('.menu'),
       menuClose = document.querySelector('.menu__close');
 
+// Hamburger 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('menu__active');
     menuClose.classList.add('active');
@@ -15,7 +16,7 @@ menuClose.addEventListener('click', () => {
     menuClose.classList.remove('active');
 });
 
-
+// dropdown
 dropDownBtn.addEventListener('click', () => {
     dropDownList.classList.toggle('menu__dropdown_list--visible');
 });
@@ -40,6 +41,8 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+
+// Swiper JS
 new Swiper('.main__slider', {
     navigation: {
         nextEl: '.swiper-button-next',
@@ -54,11 +57,43 @@ new Swiper('.main__slider', {
     slidesPerGroup: 1,
 });
 
-// async function getResponse() {
-//     let res = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
-//     let data = await res.json();
 
-//     console.log(data);
-// }
+// Getting the data from dummyjson
 
-// getResponse();
+document.addEventListener("DOMContentLoaded", () => { 
+    async function getResponse() {
+        let res = await fetch('https://dummyjson.com/products/');
+        let data = await res.json();
+        data = data.products;
+
+        let slider = document.querySelector('.main__swiper-wrapper.swiper-wrapper')
+    
+        for (let key in data) {
+            let newData = data[key];
+            // console.log(newData)
+            if (newData.price <= 50) {
+                const priceMoreThanFifty = {
+                    title: newData.title,
+                    price: newData.price
+                };
+                console.log(priceMoreThanFifty);
+                // console.log(newData.title);
+                // console.log(newData.price);
+                // for (let value in newData) {
+                //     console.log(newData[value])
+                // }
+                slider.innerHTML += `
+                    <div class="main__slide swiper-slide">
+                        <div class="main__slider-quote">“</div>
+                        <div class="main__slider-name">${priceMoreThanFifty.title}</div>
+                        <div class="main__slider-price">${priceMoreThanFifty.price}</div>
+                    </div>
+                `;
+            }
+        }
+    }
+    getResponse();
+});
+
+
+
